42329257578ecb0572bd375a6bb4adaa39d1b883
Adam Pepi <adampepi@gmail.com>
2021-01-18 14:55:01 -0800

    add version with juvenile death rate

--- t-dep pred sensitivity.R
+++ t-dep pred sensitivity.R
@@ -50,6 +50,14 @@ predprey_equations_DD3  <- function(t, y, parms) {
   dJdt <- b*A0*(1-(J0+A0)/K) - g*J0*(1-(J0+A0)/K) - a*J0*P0 - o*J0*((J0+A0)/K) #juvenile ode
   return(list(c(dAdt,dPdt,dJdt)));
 }
+predprey_equations_DD_JD  <- function(t, y, parms) {
+  a=parms[1]; c=parms[2] ; m= parms[3]; b=parms[4]; g=parms[5]; n=parms[6];o=parms[7]; K=parms[8]
+  A0 <- y[1]; P0 <- y[2]; J0 <- y[3]
+  dAdt <- g*J0 - n*A0            #adult ode
+  dPdt <- (c*a*J0*P0) - m*P0     #predator ode
+  dJdt <- b*A0*(1-(J0+A0)/K) - g*J0 - a*J0*P0 -o*J0  #juvenile ode
+  return(list(c(dAdt,dPdt,dJdt)));
+}
 
 LV.out <- lsoda(Y0, Time,predprey_equations_DD,parms)
 par(las=1,bty="l")
@@ -92,6 +100,48 @@ for(i in 1:length(as)){
 return(cbind(js,As,ps))
 }
 
+
+os<-taylor(ts,Topt = 30,Rm=1-0.3,Tsd=5)
+os<-1-os
+plot(os)
+
+tempresponse2<-function(ts, ##sequence of temperatures
+                       ToptJ,  ##topt for juveniles
+                       ToptP,  ##topt for predators
+                       Tsd=5,  ##thermal niche breadth
+                       RmP=.1, ##max predation rate
+                       Rmn=.1, ##max (?) adult death rate
+                       Rmm=.1, ##max (?) predator death rate
+                       Rmo=.1, ##max (?) juvenile rate
+                       RmJ=.4, ##max growth rate
+                       parms=parms,
+                       ODE=predprey_equations ##choose which model version to run
+)
+{
+  gs<-taylor(ts,Topt = ToptJ,Rm=RmJ,Tsd=Tsd)  ##values of G
+  as<-taylor(ts,Topt = ToptP,Rm=RmP,Tsd=Tsd)
+  ns<-taylor(ts,Topt = ToptJ,Rm=1-Rmn,Tsd=Tsd)
+  ms<-taylor(ts,Topt = ToptP,Rm=1-Rmm,Tsd=Tsd)
+  os<-taylor(ts,Topt = ToptJ,Rm=1-Rmn,Tsd=Tsd)
+  ns<-1-ns
+  ms<-1-ms
+  os<-1-os
+  
+  js<-numeric(length(as))
+  As<-numeric(length(as))
+  ps<-numeric(length(as))
+  
+  for(i in 1:length(as)){
+    
+    parms1 <- c(a=as[i],c=parms[2],m=ms[i],b=parms[4],g=gs[i],n=ns[i],o=os[i],K=parms[8] )
+    rp<-as.data.frame(steady(y=Y0,time=c(0,1e5),func=ODE,parms=parms1,method='runsteady')) #steady function from rootsolve package gets steady state
+    js[i]<-rp[1,1]
+    As[i]<-rp[2,1]
+    ps[i]<-rp[3,1]
+  }
+  return(cbind(js,As,ps))
+}
+ts<-seq(15,40,0.1)
 rt1<-tempresponse(ts,ToptJ=25,ToptP=30,Tsd=5,RmP=.1,RmJ=.4, parms=parms)
 rt1
 rt2<-tempresponse(ts,ToptJ=30,ToptP=25,Tsd=5,RmP=.1,RmJ=.4, parms=parms)
@@ -110,6 +160,16 @@ rt1DD3<-tempresponse(ts,ToptJ=25,ToptP=30,Tsd=5,RmP=.1,RmJ=.4, parms=parms,ODE=p
 rt1DD3
 rt2DD3<-tempresponse(ts,ToptJ=30,ToptP=25,Tsd=5,RmP=.1,RmJ=.4, parms=parms, ODE=predprey_equations_DD3)
 rt2DD3
+
+rt1DD<-tempresponse2(ts,ToptJ=27,ToptP=30,Tsd=5,RmP=.1,RmJ=.4,Rmo=.3,Rmn=.3,Rmm=.1, parms=parms,ODE=predprey_equations_DD_JD)
+rt1DD
+rt2DD<-tempresponse2(ts,ToptJ=30,ToptP=27,Tsd=5,RmP=.1,RmJ=.4,Rmo=.3,Rmn=.3,Rmm=.1, parms=parms, ODE=predprey_equations_DD_JD)
+rt2DD
+
+par(mfrow=c(2,1))
+matplot(x=ts, y=rt1DD, type='l', xlab="Temp", ylab="density")
+matplot(x=ts, y=rt2DD, type='l', xlab="Temp", ylab="density")
+
 
 par(mfrow=c(3,2))
 
